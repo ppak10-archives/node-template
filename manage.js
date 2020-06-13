0b9c8b180b35f932b51822dac7666c2afa9bd15e
Peter <ppak10@gmail.com>
2020-06-13 15:13:39 -0700

    Updates manage.js to use class structure

--- manage.js
+++ manage.js
@@ -9,31 +9,49 @@ const {exec} = require('child_process');
 // Constants
 const {argv, platform} = process;
 const {COMMANDS} = require('./manage.json');
-const ARGUMENT_INDEX = 2;
+const ARGUMENT_START_INDEX = 2;
 
-const manage = () => {
-  if (argv.length <= ARGUMENT_INDEX) {
-    console.error(`err: Please pass an argument`);
-  } else {
-    const command = COMMANDS[argv[ARGUMENT_INDEX]].platforms[platform];
-    run(command);
+class Manage {
+  runExec(cmd) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`err: ${err.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+
+      // eslint-disable-next-line no-console
+      console.log(`stdout: ${stdout}`);
+    });
   }
-};
 
-const run = (command) => {
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`err: ${err.message}`);
-      return;
+  getCommand() {
+    const arg = argv[ARGUMENT_START_INDEX];
+    if (COMMANDS[arg]) {
+      if (COMMANDS[arg].platforms[platform]) {
+        return COMMANDS[arg].platforms[platform];
+      } else {
+        console.error(`err: Command not avaliable for '${platform}' platform`);
+      }
+    } else {
+      console.error(`err: '${arg}' is not a recognized argument`);
     }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
+  }
+
+  start() {
+    if (argv.length <= ARGUMENT_START_INDEX) {
+      console.error(`err: Please pass an argument`);
+    } else {
+      const cmd = this.getCommand();
+      if (cmd) {
+        this.runExec(cmd);
+      }
     }
+  }
+}
 
-    // eslint-disable-next-line no-console
-    console.log(`stdout: ${stdout}`);
-  });
-};
-
-manage();
+const manage = new Manage();
+manage.start();
